8cc611b3e3ab2104efb5fccf89171c0e656aa79a
alexandra alekhina <atolkodubova@gmail.com>
2023-04-05 22:58:04 +0200

    add all

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/SashaTolkodubova/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/SashaTolkodubova/frontend-project-44/actions)
 <a href="https://codeclimate.com/github/SashaTolkodubova/frontend-project-4444/maintainability"><img src="https://api.codeclimate.com/v1/badges/ad9a72da1f33b69fb866/maintainability" /></a>
 <a href="https://asciinema.org/a/nnabWYBcNY9ZNByEVTBbVM83l" target="_blank"><img src="https://asciinema.org/a/nnabWYBcNY9ZNByEVTBbVM83l.svg" /></a>
+<a href="https://asciinema.org/a/5hSqMZLqsTaJot651Dy7VRKdc" target="_blank"><img src="https://asciinema.org/a/5hSqMZLqsTaJot651Dy7VRKdc.svg" /></a>
---
+++ bin/brain-calc.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import startGames from '../src/index.js';
+
+const randomOperatorGenerator = () => {
+  const operators = ['+', '-', '*'];
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  const operator = operators[randomIndex];
+  return operator;
+};
+
+const rule = 'What is the result of the expression?';
+
+const calculate = (num1, num2, operator) => {
+  let answer;
+  switch (operator) {
+    case '+':
+      answer = `${num1 + num2}`;
+      break;
+    case '-':
+      answer = `${num1 - num2}`;
+      break;
+    case '*':
+      answer = `${num1 * num2}`;
+      break;
+    default:
+      throw new Error(`Unknown operator: '${operator}'`);
+  }
+  return answer;
+};
+
+const startRound = () => {
+  const randomNumber1 = Math.round(Math.random() * 10);
+  const randomNumber2 = Math.round(Math.random() * 10);
+  const operator = randomOperatorGenerator();
+  const question = `Question: ${randomNumber1} ${operator} ${randomNumber2}`;
+  const correctAnswer = calculate(randomNumber1, randomNumber2, operator);
+  return [question, correctAnswer];
+};
+
+const runGame = () => startGames(rule, startRound);
+runGame();
+
+export default runGame;
