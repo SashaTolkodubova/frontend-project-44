4ca7580250f1567a34a6b158dca330077a1cb0d8
alexandra alekhina <atolkodubova@gmail.com>
2023-04-07 19:33:33 +0200

    add all

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,6 +2,11 @@ import startGames from '../index.js';
 
 const rule = 'Find the greatest common divisor of given numbers.';
 
+const generateRandomNumber = (max = 10, min = 1) => {
+  const randomized = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomized;
+};
+
 const getGreatestCommonDivisor = (number1, number2) => {
   let a = number1;
   let b = number2;
@@ -15,8 +20,8 @@ const getGreatestCommonDivisor = (number1, number2) => {
   return a.toString();
 };
 const startRound = () => {
-  const randomNumber1 = Math.round(Math.random() * 10);
-  const randomNumber2 = Math.round(Math.random() * 10);
+  const randomNumber1 = generateRandomNumber();
+  const randomNumber2 = generateRandomNumber();
   const question = `Question: ${randomNumber1} ${randomNumber2}`;
   const correctAnswer = getGreatestCommonDivisor(randomNumber1, randomNumber2);
   return [question, correctAnswer];
